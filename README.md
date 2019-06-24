5862c7bddcb7e6e637fb7a51a6b5b74eac0dbec7
sumit kumar <sumit1523@gmail.com>
2019-06-25 00:58:33 +0530

    GraphQL implmentation with first  query

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+USED 
+
+1. GraphQl
+2. Apollo Client
+3. GraphCMS
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from "react-apollo";
-
+import gql from 'graphql-tag';
 import './App.css';
 
 
 const client = new ApolloClient({
   uri: "https://api-apeast.graphcms.com/v1/cjxapd7gq0vjn01g07utz8efw/master"
 });
+
+const testQuery = gql`
+{
+  posts {
+    id
+    title
+    body
+    createdAt
+  }
+}
+`
+client.query({
+  query: testQuery
+}).then(res => console.log(res));
 
 class App extends Component {
   render(){
